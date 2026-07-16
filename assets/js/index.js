6dcd258c07fdd76b380e8ba1bc9f7f4286f7cad4
Dwi-Susilo <dwisusiloat@gmail.com>
2026-07-16 21:51:49 +0700

    Add file index

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+// Form Submission Handling
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your message. This is a frontend demo.");
+  this.reset();
+});
+
+// Navbar Scroll Effect
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.style.padding = "0.5rem 0";
+    navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,0.5)";
+  } else {
+    navbar.style.padding = "1rem 0";
+    navbar.style.boxShadow = "none";
+  }
+});
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((navlink) => {
+  navlink.addEventListener("click", () => {
+    const isActive = document.querySelector(".nav-link.active");
+
+    if (isActive) isActive.classList.remove("active");
+
+    navlink.classList.add("active");
+  });
+});
