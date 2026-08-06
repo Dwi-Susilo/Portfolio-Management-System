00adbe313e612e9949c20595a502280dd68b7d4d
Dwi-Susilo <dwisusiloat@gmail.com>
2026-08-06 13:01:47 +0700

    feat(controller): membuat controller untuk pesan masuk, lihat pesan, hapus pesan

--- controllers/messagesController.php
+++ controllers/messagesController.php
@@ -1,8 +1,62 @@
 <?php
 defined('APP_RUNNING') || abort(403);
+require_once ROOT_DIR . '/model/messages.php';
 
 function messages()
 {
     setLayout('dashboard');
-    return renderView('dashboard/messages/index');
+    return renderView('dashboard/messages/index', [
+        'messages' => getAllMessages(),
+    ]);
+}
+
+function show()
+{
+    $rawId = query('id', '');
+    $id    = decodeId($rawId);
+
+    if ($id === 0) {
+        redirect(getPath());
+    }
+
+    $messageData = getMessageById($id);
+
+    if (! $messageData) {
+        abort(404);
+    }
+
+    if ($messageData['is_read'] == 0) {
+        markAsRead($id);
+    }
+
+    setLayout('dashboard');
+
+    return renderView(getPath() . '/show', [
+        'message' => $messageData,
+    ]);
+}
+
+function delete()
+{
+    verifyCsrf();
+
+    $id = (int) ($_POST['id'] ?? 0);
+
+    if (empty($id)) {
+        redirect(getPath());
+    }
+
+    $messageData = getMessageById($id);
+
+    if (! $messageData) {
+        abort(404);
+    }
+
+    if (deleteMessage($messageData['id'])) {
+        $_SESSION['alert']['success'] = 'Pesan dari ' . $messageData['name'] . ' berhasil dihapus.';
+    } else {
+        $_SESSION['alert']['danger'] = 'Terjadi kesalahan, pesan gagal dihapus.';
+    }
+
+    redirect(getPath());
 }
